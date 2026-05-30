4ab737b625f11cfeb6a053a1ee28ac03060e5e13
IndraNurfa <ahmadindran@gmail.com>
2026-05-30 21:23:44 +0700

    fix: return createuser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repositories';
 import { UserModel } from './entities/users.entities';
+import { RegisterDto } from 'src/auth/dto/req-auth.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private usersRepository: UsersRepository) {}
 
-  async createUser(data: any) {
-    return await this.usersRepository.createUser(data);
+  async createUser(data: RegisterDto): Promise<UserModel> {
+    return await this.usersRepository.createUser(data as UserModel);
   }
 
   async findByEmail(email: string): Promise<UserModel | null> {
