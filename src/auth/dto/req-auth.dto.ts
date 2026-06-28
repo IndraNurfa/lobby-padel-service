bc124cc9f8e82e9520a0a4faad28337108f6e184
IndraNurfa <ahmadindran@gmail.com>
2026-06-29 00:17:19 +0700

    feat: add Swagger documentation for auth endpoints and data transfer objects

--- src/auth/dto/req-auth.dto.ts
+++ src/auth/dto/req-auth.dto.ts
@@ -1,35 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'johndoe@example.com' })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({ example: 'John Doe' })
   @IsString()
   full_name!: string;
 
+  @ApiProperty({ example: 'strongPassword123', minLength: 6 })
   @IsString()
   @MinLength(6)
   password!: string;
 }
 
 export class LoginDto {
+  @ApiProperty({ example: 'johndoe@example.com' })
   @IsEmail()
   email!: string;
 }
 
 export class LoginByPassDto {
+  @ApiProperty({ example: 'johndoe@example.com' })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({ example: 'strongPassword123', minLength: 6 })
   @IsString()
   @MinLength(6)
   password!: string;
 }
 
 export class VerifyOtpDto {
+  @ApiProperty({ example: 'johndoe@example.com' })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({ example: '123456' })
   @IsString()
   @MinLength(6)
   otp!: string;
--- src/auth/dto/resp-auth.dto.ts
+++ src/auth/dto/resp-auth.dto.ts
@@ -1,40 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 
 export class ResponseRegisterDto {
+  @ApiProperty({ example: 1 })
   @Expose()
   id!: number;
 
+  @ApiProperty({ example: 'johndoe@example.com' })
   @Expose()
   email!: string;
 
+  @ApiProperty({ example: 'John Doe' })
   @Expose()
   fullName!: string;
 
   @Exclude()
   password!: string;
 
+  @ApiProperty({ example: 'user' })
   @Expose()
   role!: string;
 
   @Exclude()
   createdAt!: string;
+
   @Exclude()
   updatedAt!: string;
 }
 
 export class ResponseLoginDto {
+  @ApiProperty({ example: 1 })
   @Expose()
   id!: number;
 
+  @ApiProperty({ example: 'johndoe@example.com' })
   @Expose()
   email!: string;
 
+  @ApiProperty({ example: 'John Doe' })
   @Expose()
   fullName!: string;
 
   @Exclude()
   password!: string;
 
+  @ApiProperty({ example: 'user' })
   @Expose()
   role!: string;
 
@@ -44,9 +54,17 @@ export class ResponseLoginDto {
   @Exclude()
   updatedAt!: string;
 
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30',
+  })
   @Expose()
   access_token!: string;
 
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30',
+  })
   @Expose()
   refresh_token!: string;
 }
